f69267e3a716f3d61cec65d37197db365d8eac03
IZEKOR PAUL <izekorpaul297@gmail.com>
2022-11-03 19:12:59 +0100

    feat

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,33 +1,68 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Contact() {
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+  
+  useEffect(() => {
+    window.scroll(0,0)
+  }, [])
+
+  const [data, setData] = useState({
+    fName: '',
+    lName: '',
+    email: '',
+    message: '',
+    agree: false
+})
+
+
   return (
     <div className="contact">
       <p className="head-text">Contact Me</p>
       <p className="head-text2">Hi there, contact me to ask me about anything you have in mind.</p>
 
-      <form action="" id="form">
+      <form id="form" onSubmit={handleSubmit}>
 
       <div className="username-field">
-      <label for="html">First Name
-        <input type="text" name="first_name" id="first_name"  placeholder="Enter your first name"/>
-        </label>
+      <label htmlFor="fname">First name<br />
+            <input type="text" id="first_name" name="fname" value={data?.fName} onChange={(e) => setData({
+                        ...data,
+                        fName: e.target.value
+                    })} placeholder='Enter your first name' required />
+          </label>
 
-        <label for="html">Last Name
-        <input type="text" name="last_name" id="last_name" placeholder="Enter your last name"/>
-        </label>
+          <label htmlFor="lname">Last name<br />
+            <input type="text" id="last_name" name="lname" value={data?.lName} onChange={(e) => setData({
+                        ...data,
+                        lName: e.target.value
+                    })} placeholder='Enter your last name' required />
+          </label>
       </div>
+      <label htmlFor="email">Email<br />
+            <input type="email" name="email" id="email" value={data?.email} onChange={(e) => setData({
+                        ...data,
+                        email: e.target.value
+                    })} placeholder="yourname@email.com" required />
+          </label>
 
-        <label for="html">Email</label>
-        <input type="email" name="email" id="email" placeholder="yourname@email.com"/>
+          <label htmlFor="message">Message<br />
+          <textarea name="message" id="message" value={data?.message} onChange={(e) => setData({
+                        ...data,
+                        message: e.target.value
+                    })} placeholder="Send me a message and I'll reply you as soon as possible..." required></textarea>
+        </label>
+        <label htmlFor="checkbox" className='check-wrap'>
+          <input type="checkbox" id="checkbox" checked={data?.agree} onChange={() => setData({
+                        ...data,
+                        agree: !data?.agree
+                    })} required />You agree to providing your data to Paulizekor who may contact you.
+          <span className="check"></span>
+        </label><br />
 
-        <label for="html">Message</label>
-        <textarea name="textarea" id="message" cols="30" rows="10" placeholder="Send me a message and i'll reply you as soon as possible"></textarea>
-
-        <input type="checkbox" name="checkbox" id="" />
-        <label htmlFor="html">You agree to providing your data to Paul who may contact you.</label>
-
-        <button type="submit" id="btn__submit">Send message</button>
+        <button id="btn__submit">Send message</button>
       </form>
     </div>
   )
